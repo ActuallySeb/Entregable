4fddec8064ce5bf3c52d16cd0de3141880242eaf
ActuallySeb <sebastian.montalvo3@upr.edu>
2026-04-12 21:35:04 -0400

    Tile damage fixed + Coin collision with player

--- src/bodyblock.asm
+++ src/bodyblock.asm
@@ -1,10 +1,45 @@
 .include "constants.inc"
 
 .segment "ZEROPAGE"
-.importzp player_x, player_y, enemy_x, enemy_y, player_prev_x, player_prev_y
+.importzp player_x, player_y, enemy_x, enemy_y, player_prev_x, player_prev_y, coin_x, coin_y
 
 .segment "CODE"
-.export resolve_player_enemy_bodyblock, enemy_overlaps_player
+.export resolve_player_enemy_bodyblock, enemy_overlaps_player, player_overlaps_coin
+
+.proc player_overlaps_coin
+  LDA player_x
+  CLC
+  ADC #15
+  CMP coin_x
+  BCC @no
+
+  LDA coin_x
+  CLC
+  ADC #15
+  CMP player_x
+  BCC @no
+
+  LDA player_y
+  CLC
+  ADC #15
+  CMP coin_y
+  BCC @no
+
+  LDA coin_y
+  CLC
+  ADC #15
+  CMP player_y
+  BCC @no
+
+  LDA #1
+  RTS
+
+@no:
+  LDA #0
+  RTS
+
+  RTS
+.endproc
 
 .proc enemy_overlaps_player
   LDA player_x
--- src/player.asm
+++ src/player.asm
@@ -6,7 +6,8 @@
 .importzp sprite_x, sprite_y, pads, flip_state, Collision_tiles, player_x, player_y
 
 .segment "CODE"
-.import walk_cycle, rear_cycle, forward_cycle, idle, check_player_BG_collisions, BG_collision_damage
+.import walk_cycle, rear_cycle, forward_cycle, idle, check_player_BG_collisions
+.import BG_collision_damage, randomize_coin, player_overlaps_coin
 .export update_player, move_sprite
 
 
@@ -21,6 +22,25 @@
 
   LDX #0
   JSR move_sprite
+
+  JSR player_overlaps_coin
+  BEQ @check_BG
+
+  LDA sprite_x
+  STA player_x
+
+  LDA sprite_y
+  STA player_y
+  
+  JSR randomize_coin
+
+  LDA player_x
+  STA sprite_x
+
+  LDA player_y
+  STA sprite_y
+
+  @check_BG:
   JSR check_player_BG_collisions
 
   LDX #0
@@ -42,11 +62,7 @@
     STA sprite_y
 
     LDA Collision_tiles, X
-    CMP #2
-    BEQ @exit_update
-
-    LDA Collision_tiles, X
-    CMP #3
+    CMP #1
     BEQ @exit_update
 
     JSR BG_collision_damage
