778d8da279fd64c89994837ff93ebd4a77535441
ActuallySeb <sebastian.montalvo3@upr.edu>
2026-04-12 20:31:50 -0400

    Damage works as intended; Resolve commented

--- src/bodyblock.asm
+++ src/bodyblock.asm
@@ -1,13 +1,13 @@
 .include "constants.inc"
 
 .segment "ZEROPAGE"
-.importzp sprite_x, sprite_y, enemy_x, enemy_y, player_prev_x, player_prev_y
+.importzp player_x, player_y, enemy_x, enemy_y, player_prev_x, player_prev_y
 
 .segment "CODE"
 .export resolve_player_enemy_bodyblock, enemy_overlaps_player
 
 .proc enemy_overlaps_player
-  LDA sprite_x
+  LDA player_x
   CLC
   ADC #15
   CMP enemy_x
@@ -16,10 +16,10 @@
   LDA enemy_x
   CLC
   ADC #15
-  CMP sprite_x
+  CMP player_x
   BCC @no
 
-  LDA sprite_y
+  LDA player_y
   CLC
   ADC #15
   CMP enemy_y
@@ -28,7 +28,7 @@
   LDA enemy_y
   CLC
   ADC #15
-  CMP sprite_y
+  CMP player_y
   BCC @no
 
   LDA #1
@@ -43,10 +43,10 @@
   JSR enemy_overlaps_player
   BEQ @done
 
-  LDA player_prev_x
-  STA sprite_x
-  LDA player_prev_y
-  STA sprite_y
+  ; LDA player_prev_x
+  ; STA player_x
+  ; LDA player_prev_y
+  ; STA player_y
 
 @done:
   RTS
